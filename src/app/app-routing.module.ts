55e6ee87e6c835a7828f83a6f8d8e004ee3d8c47
Priscila Sartori <priscilasartori2@gmail.com>
2023-10-19 16:11:19 -0300

    feat: Estrutura user

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './user/pages/login/login.component';
+import { HomeComponent } from './user/pages/home/home.component';
+import { ForgotPasswordComponent } from './user/pages/forgot-password/forgot-password.component';
+
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'forgotPassword', component: ForgotPasswordComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './user/pages/login/login.component';
+import { HomeComponent } from './user/pages/home/home.component';
+import { ForgotPasswordComponent } from './user/pages/forgot-password/forgot-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     BrowserModule,
